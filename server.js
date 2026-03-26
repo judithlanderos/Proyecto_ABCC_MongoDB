6ef599544d44727ec7abdc2ec679e6c5ccf5d387
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-03-25 21:16:48 -0600

    Cambio en .js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/database');
 const alumnosRoutes = require('./routes/alumnos_routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
